ca15421b8751bd8a72c1c627e7beb4598eca989f
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-12-26 11:58:54 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminLayout from './pages/admin/AdminLayout';
+import Dashboard from './pages/admin/Dashboard';
+import { CategoryList, CategoryCreate, CategoryEdit } from './pages/admin/categories';
+import PublicLayout from './layouts/PublicLayout';
+import Hero from './components/Hero';
+import ProductSection from './components/ProductSection';
+import BlogSection from './components/BlogSection';
+import Reviews from './components/Reviews';
+import ProductList from './pages/admin/products/ProductList';
+import ProductCreate from './pages/admin/products/ProductCreate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Your existing public routes */}
+        <Route path="/" element={<PublicLayout />}>
+          <Route
+            index
+            element={
+              <>
+                <Hero />
+                <ProductSection title="Kosmetyki z Serii Białej" bgColor="bg-white" />
+                <ProductSection title="Kosmetyki z Serii Białej" bgColor="bg-white" />
+                <BlogSection />
+                <Reviews />
+              </>
+            }
+          />
+          {/* Add other public routes here */}
+        </Route>
+        {/* Admin routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="categories">
+            <Route index element={<CategoryList />} />
+            <Route path="create" element={<CategoryCreate />} />
+            <Route path=":id/edit" element={<CategoryEdit />} />
+          </Route>
+          <Route path="products">
+            <Route index element={<ProductList />} />
+            <Route path="create" element={<ProductCreate />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
